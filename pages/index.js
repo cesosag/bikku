f1518378bcc8487014599aeed331f6bb0c17c404
Carlos Sosa <cesosag@gmail.com>
2021-06-10 22:52:44 +0100

    Initial and very very basic setup

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return <figure style={{ display: 'grid', height: '100vh', placeItems: 'center' }}>
+        <img src="/bikku.png" />
+    </figure>
+}
+
+export default Home
